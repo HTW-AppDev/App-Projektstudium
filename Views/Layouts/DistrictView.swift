369bfe998fa00d6771caccb9467c1a975998f7ba
GizemBiliktue <Gizembiliktu@gmail.com>
2024-06-20 20:38:04 +0200

    erster District Screen fertig

--- Views/Layouts/DistrictView.swift
+++ Views/Layouts/DistrictView.swift
@@ -19,16 +19,18 @@ struct DistrictView: View {
                 
                 VStack {
                     Spacer()
-                    HStack {
+                    HStack (alignment: .center){
+                        Spacer()
                         Button(action: {
                             // Hier den Code für das Zurücknavigieren einfügen
                         }) {
                             Image(systemName: "arrow.left")
                                 .foregroundColor(.white)
                                 .font(.title)
+                                .padding(.top, 60)
                         }
-                        .padding(.trailing, 20)
-                        
+                        Spacer()
+
                         
                         Text("KiezKumpan")
                             .font(.system(size: 40))
@@ -45,8 +47,11 @@ struct DistrictView: View {
                             Image(systemName: "house")
                                 .foregroundColor(.white)
                                 .font(.title)
+                                .padding(.top, 60)
+                                .padding(.leading, 7)
+
                         }
-                        .padding(.leading, 20)
+                        Spacer()
                         
                     }
                     
@@ -63,53 +68,67 @@ struct DistrictView: View {
                                 .padding(.top, 5)
                                 .padding(.bottom, 10)
                             
-                            HStack {
+                            HStack(spacing: 4) {
                                 Text("Eigenschaften")
                                     .padding(8)
-                                    .background(Color(red: 192/255, green: 219/255, blue: 148/255))
-                                    .cornerRadius(10)
+                                    .frame(maxWidth: 110)
+                                    .background(Color(red: 160/255, green: 190/255, blue: 100/255))
+                                    .cornerRadius(6)
+                                    .font(Font.custom("Raleway-Regular", size: 13))
+                                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 2, y: 2)
+
 
                                 
                                 Text("Highlights")
                                     .padding(8)
-                                    .background(Color(red: 79/255, green: 117/255, blue: 110/255))
+                                    .frame(maxWidth: 110)
+                                    .background(Color(red: 120/255, green: 157/255, blue: 150/255))
+                                    .cornerRadius(6)
+                                    .font(Font.custom("Raleway-Regular", size: 13))
+                                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 2, y: 2)
+
 
 
                                 Text("Compare")
                                     .padding(8)
+                                    .frame(maxWidth: 110)
                                     .background(Color(red: 79/255, green: 117/255, blue: 110/255))
-                                    .cornerRadius(10)
+                                    .cornerRadius(6)
+                                    .font(Font.custom("Raleway-Regular", size: 13))
+                                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 2, y: 2)
+
                                 
                             }
                             .foregroundColor(.white)
-                            .padding(.bottom, 5)
+                            .padding(.bottom, 10)
                             
                             VStack(alignment: .leading) {
                                 Text("Eigenschaften")
                                     .font(.system(size: 24))
                                     .kerning(3)
-                                    .padding(.top, 12)
+                                    .padding(.top, 10)
                                     .padding(.bottom, 18)
+                                    .font(Font.custom("Raleway-Regular", size: 24))
 
                                 
                                 HStack {
                                     VStack (alignment: .leading) {
                                         Text("Party Score")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("Kriminalität")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
 
                                         Text("Grünflächenanteil")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
 
                                         Text("Bademöglichkeiten pro km^2")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
 
                                         Text("Restaurants pro km2")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
 
                                         Text("Cafes pro km2")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
 
                                         Text("Sportplätze pro km2")
                                         Spacer()
@@ -122,19 +141,19 @@ struct DistrictView: View {
                                     
                                     VStack (alignment: .trailing) {
                                         Text("6")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("3")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("8,3%")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("4,2")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("1,5")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("0,3")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Text("0,5")
-                                            .padding(.bottom, 5)
+                                            .padding(.bottom, 7)
                                         Spacer()
                                     }
                                     
