09905680f15b4f4b3b5f405b7b99b1f87f1b87ee
not Ha Lan Nguyen <nothalannguyen@mac-von-Lan-632.local>
2024-06-27 12:31:11 +0200

    Map mit Liste

---
+++ Views/Components/LocationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsListView.swift
+//  KiezKompare
+//
+//  Created by not Ha Lan Nguyen on 27/6/24.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsListView()
+}
---
+++ Views/Components/MapViewDistrictWithList.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewDistrictWithList.swift
+//  KiezKompare
+//
+//  Created by not Ha Lan Nguyen on 27/6/24.
+//
+
+import SwiftUI
+
+struct MapViewDistrictWithList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapViewDistrictWithList()
+}
