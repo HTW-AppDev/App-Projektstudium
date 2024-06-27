108bb31f745f1e3a84983ce11a5601e180a1017c
not Ha Lan Nguyen <nothalannguyen@mac-von-Lan-632.local>
2024-06-27 15:54:24 +0200

    Kommentar loeschen

--- Views/Components/MapViewDistrict.swift
+++ Views/Components/MapViewDistrict.swift
@@ -56,61 +56,6 @@ struct MapViewDistrict_Previews: PreviewProvider{
         MapViewDistrict(districtID: 1)
     }
 }
-    /*class Coordinator: NSObject, MKMapViewDelegate{
-        var parent: MapViewDistrict
-        
-        init(parent: MapViewDistrict) {
-            self.parent = parent
-        }
-    }
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation:MKAnnotation) -> MKAnnotationView{
-        let identifier="marker"
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
-        
-        if annotationView == nil{
-            annotationView = MKMarkerAnnotationView(annotation:annotation,reuseIdentifier: identifier)
-            annotationView?.canShowCallout = true
-        } else{
-            annotationView?.annotation = annotation
-        }
-        return annotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(parent: self)
-    }
-    func makeUIView(context: Context) -> MKMapView{
-        let mapView = MKMapView();
-        mapView.delegate = context.coordinator
-        
-        var selectedDistrict:District?
-        
-        for district in districts {
-            if district.id == districtID {
-                selectedDistrict = district
-                break
-            }
-        }
-        guard let district  = selectedDistrict else{
-            return mapView
-        }
-        
-        let region = MKCoordinateRegion(center:district.centerCoordinate, latitudinalMeters: 2000,longitudinalMeters: 2000)
-        mapView.setRegion(region, animated: true)
-        for location in district.locations {
-            let annotaion = MKPointAnnotation()
-            annotaion.coordinate = location.coordinate
-            annotaion.title = location.name
-            annotaion.subtitle = location.address
-            mapView.addAnnotation(annotaion)
-        }
-        return mapView
-        
-    }
-    
-    
-    func updateUIView(_ uiView: MKMapView, context: Context) {}
-}*/
+
 
 
