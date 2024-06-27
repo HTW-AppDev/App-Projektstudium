26dcb7585de9d20ad36b392b6d832db1bde0d704
janinafeliciano <janina.feliciano@outlook.de>
2024-06-27 11:16:07 +0200

    Navigation Screen hinzugefügt

---
+++ Views/Layouts/NavigateView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigateView.swift
+//  KiezKompare
+//
+//  Created by Janina on 27.06.24.
+//
+
+import SwiftUI
+
+struct NavigateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigateView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigateView()
+    }
+}
